f008e33f0eb90e5ed3bbf39c8b81a88d247ae829
haywhizzz <haywhizzz@gmail.com>
2020-10-23 14:07:29 +0100

    Add friend relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,30 +9,31 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships
-  has_many :inverse_friendship, :class_name => "Friendship", :foreign_key => "friend_id"
-
+  # has_many :friends, through: :friendships
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships
+  # has_many :pending_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :inverted_friendships, source: :user
   def friends
-    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
-    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
-    friends_array.compact
+    friends_i_sent_friendship = Friendship.where(user_id: id, confirmed: true).pluck(:friend_id)
+    friends_i_got_friendship = Friendship.where(friend_id: id, confirmed: true).pluck(:user_id)
+    ids = friends_i_sent_friendship + friends_i_got_friendship
+    User.where(id: ids)
   end
 
-  def pending_friends
-    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  def friend_with?(user)
+    Friendship.confirmed_record?(id, user.id)
   end
 
-  def friend_requests
-    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
-  end
-
-  def confirm_friend(user)
-    friendship = inverse_friendships.find{|friendship| friendship.user == user}
-    friendship.confirmed = true
-    friendship.save
+  def send_friendship(user)
+    friendships.create(friend_id :user.id)
   end
 
   def friend?(user)
     friends.include?(user)
   end
+
 end
--- db/migrate/20201022121245_create_friendships.rb
+++ db/migrate/20201022121245_create_friendships.rb
@@ -2,8 +2,8 @@ class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
     create_table :friendships do |t|
       t.references :user, index: true, foreign_key: true
-      t.references :friend, index: true
-      t.boolean :confirmed
+      t.references :friend_id, index: true
+      t.boolean :confirmed, default: :false 
 
       t.timestamps
     end
